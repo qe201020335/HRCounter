ba360d29da679ef1335fe9612f9d1005f0e03065
Sky Z <qe201020335@sina.com>
2021-02-24 21:29:28 -0500

    Change web request method

---
+++ HRCounter/Data/BpmDownloader.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System.Collections;
+using System.Net;
+using HRCounter.Configuration;
+using UnityEngine.Networking;
+using IPALogger = IPA.Logging.Logger;
+
+namespace HRCounter.Data
+{
+    public class BpmDownloader
+    {
+
+        internal BPM bpm { get; } = new BPM();
+        private readonly string URL = PluginConfig.Instance.Feed_link;
+        private IPALogger log = Logger.logger;
+        private bool Log_HR = PluginConfig.Instance.Log_HR;
+        public bool updating;
+
+        public IEnumerator Updating()
+        {   
+            log.Debug("Requesting HR data");
+            while (updating)
+            {
+                yield return Update();
+            }
+        }
+
+        public IEnumerator Update()
+        {
+            using (UnityWebRequest webRequest = UnityWebRequest.Get(URL))
+            {
+                // Request and wait for the desired page.
+                yield return webRequest.SendWebRequest();
+                if (webRequest.isNetworkError)
+                {
+                    log.Error($"Error Requesting HR data: {webRequest.error}");
+                    throw new WebException();
+                }
+
+                JsonConvert.PopulateObject(webRequest.downloadHandler.text, bpm);
+                if (Log_HR)
+                {
+                    log.Debug(bpm.ToString());
+                }
+            }
+        }
+        
+        
+        
+    }
+}
--- HRCounter/HRCounter.cs
+++ HRCounter/HRCounter.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using CountersPlus.Counters.Custom;
 using HRCounter.Configuration;
-using Newtonsoft.Json;
-using System.Net;
 using HRCounter.Data;
 using TMPro;
 using IPALogger = IPA.Logging.Logger;
 using UnityEngine;
+
+
 
 namespace HRCounter
 {
@@ -15,12 +14,10 @@ namespace HRCounter
     {
 
         private readonly string URL = PluginConfig.Instance.Feed_link;
-        private bool Log_HR = PluginConfig.Instance.Log_HR;
         private IPALogger log = Logger.logger;
-        private string json_data = string.Empty;
         private TMP_Text counter;
-        private WebClient w = new WebClient();
-        private BPM bpm = new BPM();
+        private bool updating;
+        private BpmDownloader _bpmDownloader = new BpmDownloader();
 
         public override void CounterInit()
         {
@@ -29,10 +26,10 @@ namespace HRCounter
                 log.Debug("Feed link not set.");
                 return;
             }
-
+            /*
             try
-            {   // attempt to download JSON data as a string
-                json_data = w.DownloadString(URL);
+            {   
+                _bpmDownloader.Update();
             }
             catch (Exception e)
             {
@@ -40,24 +37,27 @@ namespace HRCounter
                 log.Error(e.ToString());
                 return;
             }
-
+            */
             log.Debug("Creating counter");
             counter = CanvasUtility.CreateTextFromSettings(Settings);
             counter.fontSize = 3;
             log.Debug("Starts updating");
+            _bpmDownloader.updating = true;
+            SharedCoroutineStarter.instance.StartCoroutine(_bpmDownloader.Updating());
+            updating = true;
             SharedCoroutineStarter.instance.StartCoroutine(Ticking());
         }
 
         private IEnumerator Ticking()
         {
-            while(true)
+            while(updating)
             {
                 yield return new WaitForSecondsRealtime(1);
-                Update();
-                counter.text = $"HR {bpm.Bpm}";
+                counter.text = $"HR {_bpmDownloader.bpm.Bpm}";
             }
         }
 
+        /*
         private void Update()
         {
             try
@@ -82,9 +82,12 @@ namespace HRCounter
                 
             }
         }
+        */
 
         public override void CounterDestroy()
         {
+            updating = false;
+            _bpmDownloader.updating = false;
             log.Debug("Counter destroyed");
         }
     }
