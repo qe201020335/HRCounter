844ec13f0ef205604a1e23fd40ce79056fbf197b
Sky Z <qe201020335@sina.com>
2021-09-10 22:38:07 -0400

    big oof, fix can't change settings

--- HRCounter/UI/SettingController.cs
+++ HRCounter/UI/SettingController.cs
@@ -9,24 +9,88 @@ namespace HRCounter.UI
     {
         public override string ResourceName => "HRCounter.Configuration.configMenu.bsml";
 
-        [UIValue("LogHR")] public bool LogHR => PluginConfig.Instance.LogHR;
-
-        [UIValue("Colorize")] public bool Colorize => PluginConfig.Instance.Colorize;
-
-        [UIValue("HRLow")] public int HRLow => PluginConfig.Instance.HRLow;
-
-        [UIValue("HRHigh")] public int HRHigh => PluginConfig.Instance.HRHigh;
-
-        [UIValue("HideDuringReplay")] public bool HideDuringReplay => PluginConfig.Instance.HideDuringReplay;
-
-        [UIValue("source-list-options")] public List<object> ListOptions => Utils.Utils.DataSources;
-
-        [UIValue("source-list-choice")] public string ListChoice => PluginConfig.Instance.DataSource;
-
-        [UIValue("PauseHR")] public int PauseHR => PluginConfig.Instance.PauseHR;
-
-        [UIValue("AutoPause")] public bool AutoPause => PluginConfig.Instance.AutoPause;
+        [UIValue("LogHR")]
+        public bool LogHR
+        {
+            get => PluginConfig.Instance.LogHR;
+            set
+            {
+                PluginConfig.Instance.LogHR = value;
+            }
+        }
         
+
+        [UIValue("Colorize")]
+        public bool Colorize
+        {
+            get => PluginConfig.Instance.Colorize;
+            set
+            {
+                PluginConfig.Instance.Colorize = value;
+            }
+        }
+
+        [UIValue("HRLow")]
+        public int HRLow
+        {
+            get => PluginConfig.Instance.HRLow;
+            set
+            {
+                PluginConfig.Instance.HRLow = value;
+            }
+        }
         
+        [UIValue("HRHigh")]
+        public int HRHigh
+        {
+            get => PluginConfig.Instance.HRHigh;
+            set
+            {
+                PluginConfig.Instance.HRHigh = value;
+            }
+        }
+        
+        [UIValue("HideDuringReplay")]
+        public bool HideDuringReplay
+        {
+            get => PluginConfig.Instance.HideDuringReplay;
+            set
+            {
+                PluginConfig.Instance.HideDuringReplay = value;
+            }
+        }
+        
+        [UIValue("source-list-options")]
+        public List<object> options = Utils.Utils.DataSources;
+
+        [UIValue("source-list-choice")]
+        public string listChoice
+        {
+            get => PluginConfig.Instance.DataSource;
+            set
+            {
+                PluginConfig.Instance.DataSource = value;
+            }
+        }
+        
+        [UIValue("PauseHR")]
+        public int PauseHR
+        {
+            get => PluginConfig.Instance.PauseHR;
+            set
+            {
+                PluginConfig.Instance.PauseHR = value;
+            }
+        }
+
+        [UIValue("AutoPause")]
+        public bool AutoPause
+        {
+            get => PluginConfig.Instance.AutoPause;
+            set
+            {
+                PluginConfig.Instance.AutoPause = value;
+            }
+        }
     }
 }
