4d2df5f82c0a5c2f225e40eb0a90a5dfedbbcc02
Sky Z <qe201020335@sina.com>
2022-04-29 20:28:51 -0400

    use cloudflare worker for cors workaround

--- pages/script/generator.js
+++ pages/script/generator.js
@@ -5,9 +5,7 @@ const selector = document.getElementById("source-select")
 const info_span = document.getElementById("source-info-span")
 const info_input = document.getElementById("source-info-input")
 
-const LATEST_API_LINK = "https://api.github.com/repos/qe201020335/HRCounter/releases/latest"
-const RELEASE_ZIP_NAME_REGEX = "^HRCounter-[\\d\\.]+-bs[\\d\\.]+-[0-9A-Fa-f]+\\.zip$"
-const re = new RegExp(RELEASE_ZIP_NAME_REGEX)
+const LATEST_LINK = "https://hrcounter.skyqe.workers.dev/latest"
 
 function update_info_label() {
     info_input.value = "";
@@ -50,37 +48,23 @@ button.onclick = async () => {
     }
     console.log(config)
 
-    // get the latest release first
-    const latest = await (await fetch(LATEST_API_LINK)).json();
-
-    // console.log(latest)
-
-    // find the zip file
-    const assets = latest["assets"];
-
-    console.log(assets)
-
-    const zip_asset = assets.find((cur) => {
-        return re.test(cur["name"])
-    })
-
-    const zip_name = zip_asset["name"]
-    const zip_url = zip_asset["browser_download_url"]
-    console.log(zip_name)
-    console.log(zip_url)
-
-    const zip_get_res = await fetch(zip_url)
+    const zip_get_res = await fetch(LATEST_LINK, {mode: "cors"})
     if (zip_get_res.status !== 200) {
         return
     }
 
+    const {name, data} = await zip_get_res.json();
+
+    console.log(name)
+    console.log(data)
+
     // load the zip file
-    const zip = await JSZip.loadAsync(await zip_get_res.blob())
+    const zip = await JSZip.loadAsync(atob(data))
 
     // const userdata = await
-    await zip.folder("UserData").file("HRCounter.json", config.toJSON(), null)
+    await zip.folder("UserData").file("HRCounter.json", JSON.stringify(config), null)
     // userdata.file("HRCounter.json", config.toJSON(), null)
 
     const modified = await zip.generateAsync({type:"blob"})
-    saveAs(modified, zip_name)
+    saveAs(modified, name)
 }
