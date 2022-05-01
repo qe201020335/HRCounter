8a7b83f76dff7872baa3a7f23bdce365b932e5ea
Sky Z <qe201020335@sina.com>
2022-04-30 20:13:24 -0400

    version

--- HRCounter/Properties/AssemblyInfo.cs
+++ HRCounter/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("HRCounter")]
-[assembly: AssemblyDescription("A Counters+ custom counter that displays your heart rate in game.")]
+[assembly: AssemblyDescription("A custom counter that displays your heart rate in game.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("HRCounter")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.0")]
-[assembly: AssemblyFileVersion("2.1.0")]
+[assembly: AssemblyVersion("2.1.1")]
+[assembly: AssemblyFileVersion("2.1.1")]
