f915d790a522f72333b7b7a11038c66ff63a1fc7
Sky Z <qe201020335@sina.com>
2023-01-29 14:07:57 -0500

    fix: Beatleader replay detection

--- HRCounter/Utils/Utils.cs
+++ HRCounter/Utils/Utils.cs
@@ -4,8 +4,6 @@ using System.Reflection;
 using HarmonyLib;
 using HRCounter.Configuration;
 using UnityEngine;
-using BeatLeader.Replayer;
-using Hive.Versioning;
 using IPA.Loader;
 using JetBrains.Annotations;
 
@@ -14,29 +12,20 @@ namespace HRCounter.Utils
     public static class Utils
     {
 
-        private const string BEATLEADER_MOD_ID = "BeatLeader";
-
-        private static bool? _beatleaderHasReplay = null;
-
-        internal static bool BeatLeaderHasReplay
-        {
-            get
-            {
-                _beatleaderHasReplay ??= FindEnabledPluginMetadata(BEATLEADER_MOD_ID)?.HVersion >= new Version(0, 5, 0);
-                return _beatleaderHasReplay.Value;
-            }
-        }
-
         // copied from Camera2
-        private static readonly MethodBase ScoreSaber_playbackEnabled =
+        [CanBeNull] private static readonly MethodBase ScoreSaber_playbackEnabled =
             AccessTools.Method("ScoreSaber.Core.ReplaySystem.HarmonyPatches.PatchHandleHMDUnmounted:Prefix");
+
+        [CanBeNull] private static readonly MethodBase GetBeatLeaderIsStartedAsReplay =
+            AccessTools.Property(AccessTools.TypeByName("BeatLeader.Replayer.ReplayerLauncher"), "IsStartedAsReplay")?.GetGetMethod(false);
+
         
         internal static bool IsInReplay()
         {
             // copied from Camera2
             var ssReplay = ScoreSaber_playbackEnabled != null && (bool) ScoreSaber_playbackEnabled.Invoke(null, null) == false;
 
-            var blReplay = BeatLeaderHasReplay && ReplayerLauncher.IsStartedAsReplay;
+            var blReplay = GetBeatLeaderIsStartedAsReplay != null && (bool) GetBeatLeaderIsStartedAsReplay.Invoke(null, null);
             
             return ssReplay || blReplay;
         }
