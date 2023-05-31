d26adaf4b0cb7a36ccc809ca695d3f087e7a3b84
Sky Z <qe201020335@sina.com>
2023-05-31 02:03:14 -0400

    support pulsoid oauth implicit grant

--- ConfigGenerator/src/App.js
+++ ConfigGenerator/src/App.js
@@ -1,26 +1,37 @@
 import './App.css';
 import JSZip from "jszip";
-import {useState} from "react";
+import {useRef, useState} from "react";
 import download from "./helpers";
 
 const LATEST_LINK = "https://hrcounter.skyqe.workers.dev/latest"
-const PULSOID_TOKEN_LINK = "https://pulsoid.net/oauth2/authorize?response_type=token&client_id=0025a50e-9449-4aa5-9c68-36d2903cb6a5&redirect_uri=&scope=data:heart_rate:read&state=&response_mode=web_page"
+const PULSOID_TOKEN_LINK = "https://pulsoid.net/oauth2/authorize?response_type=token&client_id=a81a9e16-2960-487d-a741-92e22b757c85&redirect_uri=https://hrcounter.skyqe.net&scope=data:heart_rate:read&state=a52beaeb-c491-4cd3-b915-16fed71e17a8"
 const PULSOID_BRO_TOKEN = "https://pulsoid.net/ui/keys"
 const PULSOID_HINT = (
     <p>
-      If you don't have a token yet, get a token <a href={PULSOID_TOKEN_LINK} target="_blank">here</a> <strong>FOR
-      FREE</strong>!
+      If you don't have a token yet, click authorize to get one. <a href={PULSOID_TOKEN_LINK}><button>Authorize</button></a>
       <br/>
       Or, you can use a manually generated token <a href={PULSOID_BRO_TOKEN} target="_blank">here</a> if you are
       a <strong>BRO</strong> .
     </p>
 )
 
+//https://hrcounter.skyqe.net/#token_type=bearer&access_token=xxxxxxxx&expires_in=2522880000&scope=data:heart_rate:read&state=yyyyyyy
 
 function App() {
+  const sourceInfoMap = useRef(new Map())
+  const queryParameters = useRef( new URLSearchParams(window.location.hash.replace("#", "?")))
 
   const [source, setSource] = useState("Pulsoid");
-  const [source_input, set_source_input] = useState("")
+  const [sourceInput, setSourceInput] = useState(queryParameters.current.get("access_token") || "")
+
+
+  function onSourceChange(e) {
+    const newSource = e.target.value
+    sourceInfoMap.current.set(source, sourceInput)
+    const input = sourceInfoMap.current.get(newSource) || ""
+    setSource(newSource)
+    setSourceInput(input)
+  }
 
   function get_info(source) {
     switch (source) {
@@ -47,18 +58,18 @@ function App() {
     switch (source) {
       case "Pulsoid":
         config = {
-          "DataSource": "Pulsoid Token", "PulsoidToken": source_input
+          "DataSource": "Pulsoid Token", "PulsoidToken": sourceInput
         }
         break
       case "HypeRate":
         config = {
-          "DataSource": "HypeRate", "HypeRateSessionID": source_input
+          "DataSource": "HypeRate", "HypeRateSessionID": sourceInput
         }
         break
       default:
         config = null
     }
-    if (source_input === "") {
+    if (sourceInput === "") {
       config = null
     }
     console.log(config)
@@ -87,9 +98,8 @@ function App() {
         <h3>DO NOT USE THIS IF YOU ALREADY HAVE DATA SOURCE CONFIGURED</h3>
         <div id="main">
           <label>
-            <span>Data Source</span>
-            <select name="Data Source" id="source-select" defaultValue={source}
-                    onChange={(e) => setSource(e.target.value)}>
+            <span>Data Source: </span>
+            <select name="Data Source" id="source-select" defaultValue={source} onChange={onSourceChange}>
               <option value="Pulsoid">Pulsoid Token</option>
               <option value="HypeRate">HypeRate</option>
             </select>
@@ -97,8 +107,7 @@ function App() {
           <br/>
           <label>
             <span id="source-info-span"> {get_info(source)} </span>
-            <input id="source-info-input" size="25" value={source_input}
-                   onChange={(e) => set_source_input(e.target.value)}/>
+            <input id="source-info-input" size="25" value={sourceInput} onChange={(e) => setSourceInput(e.target.value)}/>
           </label>
           <div id="data-source-hint"> {get_hint(source)} </div>
           <button id="submit" onClick={onclick}>Generate!</button>
