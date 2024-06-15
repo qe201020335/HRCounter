1bed41c584f19aceb14d04a6de95a833a087ded2
qe201020335 <qe201020335@sina.com>
2024-06-15 00:58:32 -0400

    Re-organize `SettingMenuController.cs` with better regions

--- HRCounter/UI/SettingMenuController.cs
+++ HRCounter/UI/SettingMenuController.cs
@@ -24,16 +24,16 @@ namespace HRCounter.UI
     {
         [Inject]
         private readonly PluginConfig _config = null!;
-        
+
         [Inject]
         private readonly SiraLog _logger = null!;
-        
+
         private string _previousDataSource = "";
-        
+
         private void RefreshConfigUi()
         {
             _logger.Trace("SettingMenuController RefreshConfigUi");
-            NotifyPropertyChanged(string.Empty);  // refresh all of them
+            NotifyPropertyChanged(string.Empty); // refresh all of them
 
             if (_previousDataSource != _config.DataSource)
             {
@@ -43,15 +43,15 @@ namespace HRCounter.UI
 
             UpdateColorText();
         }
-        
+
         private void UpdateColorText()
         {
             if (_colorVisualizerCoroutine == null)
             {
-                ColorInfoText.text = DefaultColorText;
+                _colorInfoText.text = DefaultColorText;
             }
         }
-        
+
         private void UpdateDataSourceInfoText()
         {
             var known = DataSourceManager.TryGetFromKey(_config.DataSource, out var source);
@@ -60,7 +60,7 @@ namespace HRCounter.UI
                 _dataSourceInfoText.SetText("Unknown Data Source");
                 return;
             }
-            
+
             _dataSourceInfoText.SetText("Loading Data Source Info...");
             UnityMainThreadTaskScheduler.Factory.StartNew(async () =>
             {
@@ -77,11 +77,11 @@ namespace HRCounter.UI
                 }
 
                 _dataSourceInfoText.SetText(newText);
-                await Task.Delay(500);  // no spamming the button
+                await Task.Delay(500); // no spamming the button
                 _dataSourceInfoRefreshBtn.interactable = true;
             });
         }
-        
+
         /**
          * Request a UI refresh from other threads
          */
@@ -94,7 +94,7 @@ namespace HRCounter.UI
         {
             _logger.Trace("SettingMenuController DidActivate");
             base.DidActivate(firstActivation, addedToHierarchy, screenSystemEnabling);
-            
+
             if (firstActivation)
             {
                 // no point to refresh the ui if it literally just activated and read the config
@@ -107,7 +107,7 @@ namespace HRCounter.UI
                 // config might have changed while we were inactive
                 RefreshConfigUi();
             }
-            
+
             _config.OnSettingsChanged += RequestUiRefresh;
         }
 
@@ -115,24 +115,56 @@ namespace HRCounter.UI
         {
             _logger.Trace("SettingMenuController DidDeactivate");
             _config.OnSettingsChanged -= RequestUiRefresh;
-            _previousDataSource = "";  // force a refresh next time we activate
+            _previousDataSource = ""; // force a refresh next time we activate
             if (_colorVisualizerCoroutine != null)
             {
-                VisualizeColorsBtnPressed();  // stop the visualization and reset the text
+                VisualizeColorsBtnPressed(); // stop the visualization and reset the text
             }
-            
+
             base.DidDeactivate(removedFromHierarchy, screenSystemDisabling);
         }
 
+        private string DefaultColorText =>
+            $"<color=#{ColorUtility.ToHtmlStringRGBA(_config.LowColor)}>Low</color> -> <color=#{ColorUtility.ToHtmlStringRGBA(_config.MidColor)}>Middle</color> -> <color=#{ColorUtility.ToHtmlStringRGBA(_config.HighColor)}>High</color>";
+
+        private IEnumerator VisualizeColorsCoroutine()
+        {
+            var start = _config.HRLow;
+
+            while (isActivated)
+            {
+                _colorInfoText.text = $"<size=+5><color=#{ColorUtility.ToHtmlStringRGB(RenderUtils.DetermineColor(start))}>{start}</color></size>";
+                start++;
+                if (start > _config.HRHigh)
+                {
+                    start = _config.HRLow;
+                }
+
+                yield return new WaitForSeconds(0.05f);
+            }
+        }
+
         #region UIComponents
-        
+
+        [UIComponent("color-info-text")]
+        private TMP_Text _colorInfoText = null!;
+
+        [UIComponent("data-source-info-text")]
+        private TextPageScrollView _dataSourceInfoText = null!;
+
+        [UIComponent("data-source-info-refresh-btn")]
+        private Button _dataSourceInfoRefreshBtn = null!;
+
+        #endregion
+
+        #region UIValues
+
         [UIValue("ModEnable")]
         private bool ModEnable
         {
             get => _config.ModEnable;
             set => _config.ModEnable = value;
         }
-        
 
         [UIValue("LogHR")]
         public bool LogHR
@@ -140,7 +172,6 @@ namespace HRCounter.UI
             get => _config.LogHR;
             set => _config.LogHR = value;
         }
-        
 
         [UIValue("Colorize")]
         public bool Colorize
@@ -155,7 +186,7 @@ namespace HRCounter.UI
             get => _config.HRLow;
             set => _config.HRLow = value;
         }
-        
+
         [UIValue("HRHigh")]
         public int HRHigh
         {
@@ -163,16 +194,13 @@ namespace HRCounter.UI
             set => _config.HRHigh = value;
         }
 
-        [UIComponent("color-info-text")]
-        private TMP_Text ColorInfoText = null!;
-        
         [UIValue("HideDuringReplay")]
         public bool HideDuringReplay
         {
             get => _config.HideDuringReplay;
             set => _config.HideDuringReplay = value;
         }
-        
+
         [UIValue("source-list-options")]
         public List<object> DataSourceOptions => new List<object>(DataSourceManager.DataSourceTypes.Keys);
 
@@ -182,7 +210,7 @@ namespace HRCounter.UI
             get => _config.DataSource;
             set => _config.DataSource = value;
         }
-        
+
         [UIValue("PauseHR")]
         public int PauseHR
         {
@@ -203,19 +231,13 @@ namespace HRCounter.UI
             get => _config.IgnoreCountersPlus;
             set => _config.IgnoreCountersPlus = value;
         }
-        
-        [UIComponent("data-source-info-text")]
-        private TextPageScrollView _dataSourceInfoText = null!;
-        
+
         [UIValue("NoBloom")]
         private bool NoBloom
         {
             get => _config.NoBloom;
             set => _config.NoBloom = value;
         }
-        
-        [UIComponent("data-source-info-refresh-btn")]
-        private Button _dataSourceInfoRefreshBtn = null!;
 
         [UIValue("EnableHttpServer")]
         private bool EnableHttpServer
@@ -223,74 +245,78 @@ namespace HRCounter.UI
             get => _config.EnableHttpServer;
             set => _config.EnableHttpServer = value;
         }
-        
+
         [UIValue("EnableOscServer")]
         private bool EnableOscServer
         {
             get => _config.EnableOscServer;
             set => _config.EnableOscServer = value;
         }
-        
+
         [UIValue("LowColor")]
         private Color LowColor
         {
             get => _config.LowColor;
             set => _config.LowColor = value;
         }
-        
+
         [UIValue("MidColor")]
         private Color MidColor
         {
             get => _config.MidColor;
             set => _config.MidColor = value;
         }
-        
+
         [UIValue("HighColor")]
         private Color HighColor
         {
             get => _config.HighColor;
             set => _config.HighColor = value;
         }
-        #endregion
-        
-        [UIAction("data-source-info-refresh-btn-action")]
-        private void OnDataSourceInfoRefreshBtnPressed()
-        {
-            UpdateDataSourceInfoText();
-        }
-        
-        [UIAction("reset-low-color")]
-        private void ResetLowColor()
-        {
-            _config.LowColor = PluginConfig.DefaultValues.LowColor;
-        }
-        
-        [UIAction("reset-mid-color")]
-        private void ResetMidColor()
-        {
-            _config.MidColor = PluginConfig.DefaultValues.MidColor;
-        }
-        
-        [UIAction("reset-high-color")]
-        private void ResetHighColor()
-        {
-            _config.HighColor = PluginConfig.DefaultValues.HighColor;
-        }
-        
-        private Coroutine? _colorVisualizerCoroutine;
-        
+
         private string _visualizeColorsBtnText = "Visualize";
-        
+
         [UIValue("visualize-colors-btn-text")]
-        private string VisualizeColorsBtnText { 
+        private string VisualizeColorsBtnText
+        {
             get => _visualizeColorsBtnText;
             set
             {
                 _visualizeColorsBtnText = value;
                 NotifyPropertyChanged();
-            } 
+            }
         }
-        
+
+        #endregion
+
+        #region UIActions
+
+        [UIAction("data-source-info-refresh-btn-action")]
+        private void OnDataSourceInfoRefreshBtnPressed()
+        {
+            UpdateDataSourceInfoText();
+        }
+
+        [UIAction("reset-low-color")]
+        private void ResetLowColor()
+        {
+            _config.LowColor = PluginConfig.DefaultValues.LowColor;
+        }
+
+        [UIAction("reset-mid-color")]
+        private void ResetMidColor()
+        {
+            _config.MidColor = PluginConfig.DefaultValues.MidColor;
+        }
+
+        [UIAction("reset-high-color")]
+        private void ResetHighColor()
+        {
+            _config.HighColor = PluginConfig.DefaultValues.HighColor;
+        }
+
+        private Coroutine? _colorVisualizerCoroutine;
+
         [UIAction("visualize-colors-btn-pressed")]
         private void VisualizeColorsBtnPressed()
         {
@@ -298,7 +324,7 @@ namespace HRCounter.UI
             {
                 StopCoroutine(_colorVisualizerCoroutine);
                 _colorVisualizerCoroutine = null;
-                ColorInfoText.text = DefaultColorText;
+                _colorInfoText.text = DefaultColorText;
                 VisualizeColorsBtnText = "Visualize";
             }
             else
@@ -308,23 +334,6 @@ namespace HRCounter.UI
             }
         }
 
-        private string DefaultColorText =>
-            $"<color=#{ColorUtility.ToHtmlStringRGBA(_config.LowColor)}>Low</color> -> <color=#{ColorUtility.ToHtmlStringRGBA(_config.MidColor)}>Middle</color> -> <color=#{ColorUtility.ToHtmlStringRGBA(_config.HighColor)}>High</color>";
-
-        private IEnumerator VisualizeColorsCoroutine()
-        {
-            var start = _config.HRLow;
-
-            while (isActivated)
-            {
-                ColorInfoText.text = $"<size=+5><color=#{ColorUtility.ToHtmlStringRGB(RenderUtils.DetermineColor(start))}>{start}</color></size>";
-                start++;
-                if (start > _config.HRHigh)
-                {
-                    start = _config.HRLow;
-                }
-                yield return new WaitForSeconds(0.05f);
-            }
-        }
+        #endregion
     }
 }
