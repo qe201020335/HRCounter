3a22846fe7353c884844f7ed0f27f25e4a0002bc
qe201020335 <qe201020335@sina.com>
2024-06-23 15:19:34 -0400

    config generator add react buddy

---
+++ ConfigGenerator/src/dev/index.js
@@ -0,0 +1,9 @@
+﻿import React from "react"
+import {useInitial} from "./useInitial"
+
+const ComponentPreviews = React.lazy(() => import("./previews"))
+
+export {
+    ComponentPreviews,
+    useInitial
+}
--- ConfigGenerator/src/index.js
+++ ConfigGenerator/src/index.js
@@ -5,14 +5,18 @@ import App from './App';
 import {CssBaseline} from "@mui/material";
 import {ThemeProvider} from '@mui/material/styles';
 import {theme} from "./theme";
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "./dev";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-      <ThemeProvider theme={theme}>
-        <CssBaseline/>
-        <App/>
-      </ThemeProvider>
+        <ThemeProvider theme={theme}>
+            <CssBaseline/>
+            <DevSupport ComponentPreviews={ComponentPreviews} useInitialHook={useInitial}>
+                <App/>
+            </DevSupport>
+        </ThemeProvider>
     </React.StrictMode>
 );
