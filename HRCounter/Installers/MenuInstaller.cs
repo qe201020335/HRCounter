8a40ec27206b087739d687048bda4e6e30f0168e
qe201020335 <qe201020335@sina.com>
2024-08-30 20:26:10 -0400

    Add bsml as required dependency

--- HRCounter/Installers/MenuInstaller.cs
+++ HRCounter/Installers/MenuInstaller.cs
@@ -9,15 +9,7 @@ namespace HRCounter.Installers
         
         public override void InstallBindings()
         {
-            if (Plugin.BSMLMeta != null)
-            {
-                _logger.Debug("BSML is installed, installing the menus");
-                Container.Install<BSMLInstaller>();
-            }
-            else
-            {
-                _logger.Warn("BSML is not installed, not installing the menus");
-            }
+            Container.Install<BSMLInstaller>();
         }
     }
 }
