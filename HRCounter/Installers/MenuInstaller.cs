5d9189c100befed08a63a4175e123e677dc2e4a1
qe201020335 <qe201020335@sina.com>
2024-09-08 23:14:14 -0400

    Revert "Add bsml as required dependency"
    This reverts commit 8a40ec27206b087739d687048bda4e6e30f0168e.

--- HRCounter/Installers/MenuInstaller.cs
+++ HRCounter/Installers/MenuInstaller.cs
@@ -9,7 +9,15 @@ namespace HRCounter.Installers
         
         public override void InstallBindings()
         {
-            Container.Install<BSMLInstaller>();
+            if (Plugin.BSMLMeta != null)
+            {
+                _logger.Debug("BSML is installed, installing the menus");
+                Container.Install<BSMLInstaller>();
+            }
+            else
+            {
+                _logger.Warn("BSML is not installed, not installing the menus");
+            }
         }
     }
 }
