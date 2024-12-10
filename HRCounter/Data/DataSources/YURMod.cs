cd22045196939bc0e8744e256688075194a86448
qe201020335 <qe201020335@sina.com>
2024-12-09 21:33:35 -0500

    Reference the YUR unity sdk instead of the YUR mod

--- HRCounter/Data/DataSources/YURMod.cs
+++ HRCounter/Data/DataSources/YURMod.cs
@@ -13,28 +13,10 @@ namespace HRCounter.Data.DataSources
 
         private void OnOverlayStatusUpdate(OverlayStatusUpdate osu)
         {
-            try
+            var hr = osu.HeartRate ?? osu.CalculationMetrics?.EstHeartRate;
+            if (hr != null)
             {
-                if (osu.HeartRate == null)
-                {
-                    var calMetric = typeof(OverlayStatusUpdate).GetProperty("CalculationMetrics")?.GetValue(osu);
-                    var hr = calMetric?.GetType().GetProperty("EstHeartRate")?.GetValue(calMetric);
-                    if (hr != null)
-                    {
-                        OnHeartRateDataReceived(Convert.ToInt32(hr));
-                    }
-                }
-                else
-                {
-                    OnHeartRateDataReceived(Convert.ToInt32(osu.HeartRate.Value));
-                }
-            }
-            catch (Exception e)
-            {
-                // we don't want ANY uncaught exception,
-                // YUR invokes the action on the freaking MAIN THREAD!!!
-                Logger.Critical($"Exception occured while handling YUR Mod Status Update: {e.Message}");
-                Logger.Critical(e);
+                OnHeartRateDataReceived(Convert.ToInt32(hr.Value));
             }
         }
         
