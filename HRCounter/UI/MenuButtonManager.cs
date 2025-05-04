468e3b2d99e218f51a7a1b5532ed4a0076318715
qe201020335 <qe201020335@sina.com>
2025-05-03 23:58:48 -0400

    Remove code used for legacy BSML

--- HRCounter/UI/MenuButtonManager.cs
+++ HRCounter/UI/MenuButtonManager.cs
@@ -1,52 +1,29 @@
-﻿using System;
-using BeatSaberMarkupLanguage.MenuButtons;
-using SiraUtil.Logging;
+﻿using BeatSaberMarkupLanguage.MenuButtons;
 using Zenject;
 
 namespace HRCounter.UI;
 
-public class MenuButtonManager : IInitializable, IDisposable
+internal class MenuButtonManager : IInitializable
 {
-    private readonly MenuButton _menuButton;
-    private readonly MainFlowCoordinator _mainFlowCoordinator;
-    private readonly ConfigViewFlowCoordinator _configViewFlowCoordinator;
-    private readonly MenuButtons? _menuButtons;
+    [Inject]
+    private readonly MainFlowCoordinator _mainFlowCoordinator = null!;
 
     [Inject]
-    private readonly SiraLog _logger = null!;
+    private readonly ConfigViewFlowCoordinator _configViewFlowCoordinator = null!;
 
-    public MenuButtonManager(MainFlowCoordinator mainFlowCoordinator, ConfigViewFlowCoordinator configViewFlowCoordinator,
-        [InjectOptional] MenuButtons? menuButtons)
+    [Inject]
+    private readonly MenuButtons _menuButtons = null!;
+
+    private readonly MenuButton _menuButton;
+
+    public MenuButtonManager()
     {
-        _mainFlowCoordinator = mainFlowCoordinator;
-        _configViewFlowCoordinator = configViewFlowCoordinator;
-        _menuButtons = menuButtons;
-
         _menuButton = new MenuButton("HRCounter", "Display your heart rate in game!", OnMenuButtonClick);
     }
 
     public void Initialize()
     {
-        if (_menuButtons != null)
-        {
-            _menuButtons.RegisterButton(_menuButton);
-        }
-        else
-        {
-            _logger.Warn("BSML MenuButtons is null");
-        }
-    }
-
-    public void Dispose()
-    {
-        if (_menuButtons != null)
-        {
-            _menuButtons.UnregisterButton(_menuButton);
-        }
-        else
-        {
-            _logger.Warn("BSML MenuButtons is null");
-        }
+        _menuButtons.RegisterButton(_menuButton);
     }
 
     private void OnMenuButtonClick()
