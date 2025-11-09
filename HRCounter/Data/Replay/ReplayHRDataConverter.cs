6511a2f8f77a326c81827bf9d7af113bb19c104a
qe201020335 <qe201020335@sina.com>
2025-11-09 01:48:56 -0500

    Use BitConverter to decode replay data so it can work with the byte array directly

--- HRCounter/Data/Replay/ReplayHRDataConverter.cs
+++ HRCounter/Data/Replay/ReplayHRDataConverter.cs
@@ -17,25 +17,29 @@ internal static class ReplayHRDataConverter
         File.WriteAllBytes("debug_hrdata_parsed.bin", data);
 #endif
 
+        if (!BitConverter.IsLittleEndian)
+        {
+            // most systems are little endian nowadays
+            Logger.Error("Big Endian systems are not supported");
+            return null;
+        }
+
         if (data.Length < 8)
         {
             Logger.Error("Data length is too short to contain valid HR data");
             return null;
         }
 
-        using var ms = new MemoryStream(data);
-        // BinaryReader is always little-endian
-        using var reader = new BinaryReader(ms);
-
-        var version = reader.ReadInt32();
+        var offset = 0;
+        var version = DecodeInt32(data, ref offset);
         if (version != 1)
         {
             Logger.Error($"Unsupported HR data version: {version}");
             return null;
         }
 
-        var entryCount = reader.ReadInt32();
-        if (data.Length < 8 + entryCount * 8)
+        var entryCount = DecodeInt32(data, ref offset);
+        if (data.Length < offset + entryCount * 8)
         {
             Logger.Error($"Data length is too short for {entryCount} entries");
             return null;
@@ -44,8 +48,8 @@ internal static class ReplayHRDataConverter
         var entries = new ReplayHR[entryCount];
         for (var i = 0; i < entryCount; i++)
         {
-            var songTime = reader.ReadSingle();
-            var heartRate = reader.ReadInt32();
+            var songTime = DecodeFloat(data, ref offset);
+            var heartRate = DecodeInt32(data, ref offset);
             entries[i] = new ReplayHR
             {
                 SongTime = songTime,
@@ -53,58 +57,71 @@ internal static class ReplayHRDataConverter
             };
         }
 
-        var deviceNameSizeOffset = 8 + entryCount * 8;
-        string deviceName;
-        int deviceNameSize;
-        if (data.Length < deviceNameSizeOffset + 4)
+        string? deviceName;
+        if (data.Length < offset + 4)
         {
             Logger.Warn("Failed to read device name size, data might be corrupted");
             deviceName = "Unknown Device";
-            return new ReplayHRData(entries, deviceName, "");
+        }
+        else if ((deviceName = DecodeString(data, ref offset)) == null)
+        {
+            deviceName = "Unknown Device";
+            Logger.Warn("Failed to read device name, data might be corrupted");
         }
 
-        if ((deviceNameSize = reader.ReadInt32()) == 0)
+        string? hrAgent;
+        if (data.Length < offset + 4)
         {
-            deviceName = "";
-        }
-        else
-        {
-            try
-            {
-                var deviceNameBytes = reader.ReadBytes(deviceNameSize);
-                deviceName = Encoding.UTF8.GetString(deviceNameBytes);
-            }
-            catch (Exception e)
-            {
-                deviceName = "Unknown Device";
-                Logger.Warn("Failed to read device name, data might be corrupted");
-                Logger.Warn(e);
-            }
-        }
-
-        var hrAgentSizeOffset = deviceNameSizeOffset + 4 + deviceNameSize;
-        int hrAgentSize;
-        string hrAgent;
-        if (data.Length < hrAgentSizeOffset + 4 || (hrAgentSize = reader.ReadInt32()) == 0)
-        {
+            // optional data
             hrAgent = "";
         }
-        else
+        else if ((hrAgent = DecodeString(data, ref offset)) == null)
         {
-            try
-            {
-                var hrAgentBytes = reader.ReadBytes(hrAgentSize);
-                hrAgent = Encoding.UTF8.GetString(hrAgentBytes);
-            }
-            catch (Exception e)
-            {
-                hrAgent = "Unknown";
-                Logger.Warn("Failed to read hr agent, data might be corrupted");
-                Logger.Warn(e);
-            }
+            hrAgent = "Unknown";
+            Logger.Warn("Failed to read hr agent, data might be corrupted");
         }
 
         return new ReplayHRData(entries, deviceName, hrAgent);
+    }
+
+    private static int DecodeInt32(byte[] data, ref int offset)
+    {
+        var result = BitConverter.ToInt32(data, offset);
+        offset += 4;
+        return result;
+    }
+
+    private static float DecodeFloat(byte[] data, ref int offset)
+    {
+        var result = BitConverter.ToSingle(data, offset);
+        offset += 4;
+        return result;
+    }
+
+    private static string? DecodeString(byte[] data, ref int offset)
+    {
+        var size = DecodeInt32(data, ref offset);
+        if (size == 0) return "";
+        if (data.Length < offset + size)
+        {
+            Logger.Warn("Data length is too short to decode string");
+            return null;
+        }
+
+        string? result;
+        try
+        {
+            result = Encoding.UTF8.GetString(data, offset, size);
+            offset += size;
+        }
+        catch (Exception e)
+        {
+            result = null;
+            Logger.Warn("Failed to decode string");
+            Logger.Warn(e);
+        }
+
+        return result;
     }
 
     public static byte[] ToBytes(ReplayHRData data)
